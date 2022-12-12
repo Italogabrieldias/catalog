8a36c8ab3f9acdb0cba6616f1c55e95e1103c9f6
Italo Gabriel <italogabrielde@gmail.com>
2022-12-12 20:56:02 -0300

    DTO

--- backend/src/main/java/com/project/catalog/catalog/services/CategoryService.java
+++ backend/src/main/java/com/project/catalog/catalog/services/CategoryService.java
@@ -1,23 +1,28 @@
 package com.project.catalog.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.project.catalog.catalog.dto.CategoryDTO;
 import com.project.catalog.catalog.entities.Category;
 import com.project.catalog.catalog.repositories.CategoryRepository;
 
-@Service 
+@Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	@Transactional
-	public List<Category> findAll() {
-		return repository.findAll();
+
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 		
 	}
 }
