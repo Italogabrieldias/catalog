861afe59a746976ebf26368e2abd38b5d442f84b
Italo Gabriel <italogabrielde@gmail.com>
2022-12-12 19:36:18 -0300

    database seeding

--- backend/src/main/java/com/project/catalog/catalog/services/CategoryService.java
+++ backend/src/main/java/com/project/catalog/catalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.catalog.catalog.entities.Category;
 import com.project.catalog.catalog.repositories.CategoryRepository;
@@ -14,7 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	@Autowired
+	@Transactional
 	public List<Category> findAll() {
 		return repository.findAll();
 		
